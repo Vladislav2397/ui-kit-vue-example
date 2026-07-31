911112af1cb498d1cafea77506780f8828b2c560
Владислав Трегубов <valdiszz53@gmail.com>
2026-07-31 09:18:50 +0300

    Added Chip component

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { UiButton } from './components/Button'
 export type { ButtonProps, ButtonSize, ButtonVariant } from './components/Button'
+
+export { UiChip } from './components/Chip'
+export type { ChipProps, ChipSize, ChipVariant } from './components/Chip'
